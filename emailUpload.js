a95b4be08e655d6bc615e8bec84f9fc1db1e7ee8
Max Gerlach <mgerlach@klick.com>
2019-04-01 17:02:45 -0400

    day end push

--- emailUpload.js
+++ emailUpload.js
@@ -1,21 +1,23 @@
-var secret = JSON.parse(fs.readFileSync("credentials.json", "utf8"));
-var emailData = JSON.parse(fs.readFileSync("veeva.json", "utf8"));
-let validate = require('validate-fields');
+
+let validate = require('validate-fields')();
 const assert = require('assert');
+const fs = require('fs');
+const vault = require("veeva-vault");
 
 ///////////////////////////////////
 // Data Structure for Validation //
 ///////////////////////////////////
 
 var EmailTemplate = {
+    version: "in(2019-04-01)",
     file: String,
     "name__v": String,
-    "lifecycle__v": "Approved Email",
-    "type__v": "Email Template",
+    "lifecycle__v": "in(Approved Email)",
+    "type__v": "in(Email Template)",
     "document_id?": String,
     "product__v": String,
     "country__v": String,
-    "restrict_fragments_by_product__v": true,
+    "restrict_fragments_by_product__v": Boolean,
     "from_name__v": String,
     "from_address__v": String,
     "reply_to_name__v": String,
@@ -26,12 +28,18 @@ var EmailTemplate = {
 var EmailFragment = {
     file: String,
     "name__v": String,
-    "lifecycle__v": "Approved Email",
-    "type__v": "Email Fragment",
+    "lifecycle__v": "in(Approved Email)",
+    "type__v": "in(Email Fragment)",
     "document_id?": String,
     "product__v": String,
     "country__v": String
 }
+
+var assertValid = (schema, value) => {
+    if(! validate(schema, value)){
+        console.error(validate.lastError);
+        console.log(value)
+        throw Error(validate.lastError)}}
 
 /////////////////////////
 // Data normalization  //
@@ -43,11 +51,12 @@ var normalizeEmailTemplate = function (origEmailTemplateData) {
     emailTemplateData["document_id"] =  emailTemplateData["document_id"] || null;
     emailTemplateData["lifecycle__v"] =  emailTemplateData["lifecycle__v"] || "Approved Email";
     emailTemplateData["type__v"] =  emailTemplateData["type__v"] || "Email Template";
-    assert(validate(EmailTemplate, emailTemplateData));
+
+    assertValid(EmailTemplate, emailTemplateData);
     return emailTemplateData;
 }
 
-var normalizeEmailFragment = function(origFragmentData, emailTemplateData) {
+var normalizeEmailFragment = function(emailTemplateData, origFragmentData) {
     var fragmentData = Object.assign({}, origFragmentData); // make a copy
 
     fragmentData["document_id"] = fragmentData["document_id"] || null;
@@ -56,10 +65,32 @@ var normalizeEmailFragment = function(origFragmentData, emailTemplateData) {
     fragmentData["product__v"] = fragmentData["product__v"] || emailTemplateData["product__v"];
     fragmentData["country__v"] = fragmentData["country__v"] || emailTemplateData["country__v"];
 
-    assert(validate(EmailFragment, fragmentData));
+    assertValid(EmailFragment, fragmentData);
     return fragmentData;
 }
 
 var normalizeData = function(data) {
-
+    var emailData = normalizeEmailTemplate(data);
+    emailData["relations"] = data["relations"].map(fragment => normalizeEmailFragment(emailData, fragment))
+    return emailData;
 }
+
+//////////
+// Main //
+//////////
+
+var secret = JSON.parse(fs.readFileSync("credentials.json", "utf8"));
+var emailData = JSON.parse(fs.readFileSync("veeva.json", "utf8"));
+
+var normalizedEmailData = normalizeData(emailData)
+
+
+vault.authenticate(secret)
+.then(function() {
+    return vault.getProducts();
+}).then(function(products) {
+    console.log("Found these products:", products);
+    return vault.getBinders();
+}).catch(function(e) {
+        console.log("Error using Vault API:", e.message);
+});
