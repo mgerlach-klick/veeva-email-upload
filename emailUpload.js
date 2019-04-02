4bd85a3a8d5a1966428e49ef23500267cfb4871a
Max Gerlach <mgerlach@klick.com>
2019-04-02 11:59:21 -0400

    Successfully update the template

--- emailUpload.js
+++ emailUpload.js
@@ -8,25 +8,29 @@ const vault = require("veeva-vault");
 // Data Structure for Validation //
 ///////////////////////////////////
 
+let BooleanString =  "in(true, false)";
+
 var EmailTemplate = {
     version: "in(2019-04-01)",
-    file: String,
+    filepath: String,
+    file: Object, // fs.ReaddStream from filepath
     "name__v": String,
     "lifecycle__v": "in(Approved Email)",
     "type__v": "in(Email Template)",
     "document_id?": String,
     "product__v": String,
     "country__v": String,
-    "restrict_fragments_by_product__v": Boolean,
+    "restrict_fragments_by_product__v?": BooleanString,
     "from_name__v": String,
     "from_address__v": String,
     "reply_to_name__v": String,
     "reply_to_address__v": String,
-    "template_subject__v": String
+    "subject__v": String
 }
 
 var EmailFragment = {
-    file: String,
+    filepath: String,
+    file: Object, // fs.ReaddStream from filepath
     "name__v": String,
     "lifecycle__v": "in(Approved Email)",
     "type__v": "in(Email Fragment)",
@@ -52,6 +56,8 @@ var normalizeEmailTemplate = function (origEmailTemplateData) {
     emailTemplateData["lifecycle__v"] =  emailTemplateData["lifecycle__v"] || "Approved Email";
     emailTemplateData["type__v"] =  emailTemplateData["type__v"] || "Email Template";
 
+    emailTemplateData["file"] = fs.createReadStream(emailTemplateData["filepath"])
+
     assertValid(EmailTemplate, emailTemplateData);
     return emailTemplateData;
 }
@@ -64,6 +70,7 @@ var normalizeEmailFragment = function(emailTemplateData, origFragmentData) {
     fragmentData["type__v"] =  fragmentData["type__v"] || "Email Template";
     fragmentData["product__v"] = fragmentData["product__v"] || emailTemplateData["product__v"];
     fragmentData["country__v"] = fragmentData["country__v"] || emailTemplateData["country__v"];
+    fragmentData["file"] = fs.createReadStream(fragmentData["filepath"])
 
     assertValid(EmailFragment, fragmentData);
     return fragmentData;
@@ -75,6 +82,19 @@ var normalizeData = function(data) {
     return emailData;
 }
 
+////////////////////////
+// Creation functions //
+////////////////////////
+
+
+var createEmailTemplate = function (emailTemplate) {
+    delete emailTemplate.relations;
+    // delete emailTemplate.document_id;
+    // console.log(emailTemplate)
+    var creationPromise = vault.createDocument(emailTemplate);
+    return creationPromise;
+}
+
 //////////
 // Main //
 //////////
@@ -83,14 +103,15 @@ var secret = JSON.parse(fs.readFileSync("credentials.json", "utf8"));
 var emailData = JSON.parse(fs.readFileSync("veeva.json", "utf8"));
 
 var normalizedEmailData = normalizeData(emailData)
+// console.log(normalizedEmailData)
 
+// upload stuf!!!
+// vault.verbose(true);
 
 vault.authenticate(secret)
-.then(function() {
-    return vault.getProducts();
-}).then(function(products) {
-    console.log("Found these products:", products);
-    return vault.getBinders();
-}).catch(function(e) {
+    .then(function() {
+        return createEmailTemplate(normalizedEmailData)
+    }).then( ret => { console.log(ret); return ret})
+    .catch(function(e) {
         console.log("Error using Vault API:", e.message);
-});
+    });
